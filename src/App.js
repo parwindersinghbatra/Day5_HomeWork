5f366b89d17c99fe181dff10976f33ef1b0b7ef8
parwindersinghbatra <parwinder.singhbatra@outlook.com>
2024-03-11 12:10:39 -0400

    Day 5 HomeWork

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [GenerateData , SetGenerateData] = useState([])
+  const InputValue = (event) => {
+    console.log(event.target.value)
+    SetGenerateData(event.target.value)
+  };
+  const GenerateParagraph = () => {
+
+    const data = [
+        "This is first Paragraph", 
+        "This is Second Paragraph", 
+        "This is third Paragraph"
+     ]
+
+    //  SetGenerateData(data.splice(0, paraValue-1, 0))
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+          <input type='text' id='paragraphNumber' onChange={InputValue}
+          />
+          <button onClick={GenerateParagraph}>Submit</button>
+        </div>
+    </>
+
   );
 }
-
 export default App;
